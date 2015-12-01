b8f0c359b19b2f2e0b72080f98af8ccff856ec02
Wai Hwa Tan <twaih92@gmail.com>
2015-12-01 15:06:43 -0800

    no longer just getting top posts. planning to get all posts from HN

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var Hapi = require('hapi');
-var http = require('http');
-var Promise = require('bluebird');
-//var request = require('request');
-
-// problem : the API presents the posts in the form of ids. in order to get the text, urls, etc
-// we'd need to make as many request as there are posts.
-
-var request = Promise.promisifyAll(require('request'));
+var request = require('request');
+//var Promise = require('bluebird');
+//var request = Promise.promisifyAll(require('request'));
 
 var server = new Hapi.Server();
 
@@ -15,37 +10,13 @@ server.connection({
   port: 3000
 });
 
+generateDatabase();
+
 server.route({
   method: 'GET',
   path: '/',
   handler: function (req, reply) {
-
-    //Makes request to Hacker News API for top news items
-    request.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', function(err, response, body) {
-        if (!err && response.statusCode === 200) {
-          var body = JSON.parse(body);
-          // 500 is the default #responses returned by the API.
-          // TODO: Find out how to limit API request
-          var results = body.slice(500-5);
-          console.log(results);
-
-          results.forEach(function (elem) {
-            console.log(elem);
-          });
-
-        }
-      });
-    // request.getAsync('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
-      // .then(function(result) {
-      //     console.log('yo', result.req.res.request);
-      //     if (!err && res.statusCode === 200) {
-      //       var body = JSON.parse(body);
-      //       // 500 is the default #responses returned by the API.
-      //       // TODO: Find out how to limit API request
-      //       results = body.slice(500-5);
-      //       console.log(results);
-      //     }
-      //   });
+    // Eventually serve analysis of trends
     reply('Hello, world!');
   }
 });
@@ -53,3 +24,12 @@ server.route({
 server.start(function () {
   console.log('Server running on: ', server.info.uri);
 });
+
+function generateDatabase() {
+  for (var i = 121003; i < 121010; i++) {
+    request.get(`https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`, function (err, response, body) {
+      var body = JSON.parse(body);
+      console.log(body);
+    });
+  }
+}
